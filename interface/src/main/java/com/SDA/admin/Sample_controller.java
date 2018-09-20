df377d8fd11b8a04e0c0b8d391ea3ecc32f99854
Paul Rondreux <p.rondreux@noos.fr>
2018-09-20 19:33:02 +0200

    commit encodage ffmpeg et test stream bytes

--- interface/src/main/java/com/SDA/admin/Sample_controller.java
+++ interface/src/main/java/com/SDA/admin/Sample_controller.java
@@ -3,8 +3,13 @@ package com.SDA.admin;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
 
 import javafx.application.Platform;
 import javafx.concurrent.Task;
@@ -48,32 +53,72 @@ public class Sample_controller implements Initializable {
     Task t = new Task() {
       @Override
       protected Object call() {
-        while (!stoppped) {
-          try {
-            System.out.println("Thread start..");
-            Robot ro = new Robot();
-            BufferedImage im = ro
-                .createScreenCapture(new java.awt.Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-            final WritableImage image = new WritableImage(500, 500);
-            final Image myPic = SwingFXUtils.toFXImage(im, null);
+        // while (!stoppped) {
+        try {
+          System.out.println("Thread start..");
+          Robot ro = new Robot();
+          BufferedImage im = ro
+              .createScreenCapture(new java.awt.Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+          // final WritableImage image = new WritableImage(500, 500);
+          // final Image myPic = SwingFXUtils.toFXImage(im, null);
 
-            // Calling the JavaFX Thread to update javafx control
-            Platform.runLater(new Runnable() {
-              public void run() {
-                // This updates the imageview to newly created Image
-                System.out.println("Print toto");
+          // WritableRaster raster = im.getRaster();
+          // DataBufferByte buffer = (DataBufferByte) raster.getDataBuffer();
+          // byte[] bytes = buffer.getData();
 
-                screen_display.setImage(myPic);
-              }
-            });
+          ByteArrayOutputStream baos = new ByteArrayOutputStream();
+          ImageIO.write(im, "jpg", baos);
+          baos.flush();
+          byte[] imageInByte = baos.toByteArray();
+          baos.close();
 
-            // Sleep for 100 millisecond
-            Thread.sleep(1000 / 30);
+          System.out.println(imageInByte);
 
-          } catch (Exception ex) {
-            // print stack trace or do other stuffs
-          }
+          // final String base64String = imgToBase64String(im, "png");
+          // System.out.println(base64String);
+          // byte[] imageBytes = ((DataBufferByte)
+          // im.getData().getDataBuffer()).getData();
+
+          // Runtime runtime = Runtime.getRuntime();
+          System.out.println("Print ddd");
+          ProcessBuilder pb = new ProcessBuilder("ffmpeg", "-i", "pipe:0", "-c:v", "libx264", "-preset", "ultrafast",
+              "-crf", "0", "pipe:1");
+          Process p = pb.start();
+          System.out.println("Print eaea");
+          p.getOutputStream().write(imageInByte);
+
+          byte[] b = new byte[imageInByte.length];
+          p.getInputStream().read(b);
+          System.out.println(b);
+
+          InputStream out = new ByteArrayInputStream(b);
+          BufferedImage bImageFromConvert = ImageIO.read(out);
+
+          final WritableImage image = new WritableImage(500, 500);
+          final Image myPic = SwingFXUtils.toFXImage(bImageFromConvert, null);
+
+          // byte[] imageInByte= new BigInteger(hex, 16).toByteArray();
+          // InputStream in = new ByteArrayInputStream(imageInByte);
+          // BufferedImage bImageFromConvert = ImageIO.read(in);
+          // p.
+
+          // Calling the JavaFX Thread to update javafx control
+
+          Platform.runLater(new Runnable() {
+            public void run() { // This updates the imageview to newly created Image
+              System.out.println("Print toto");
+
+              screen_display.setImage(myPic);
+            }
+          });
+
+          // Sleep for 100 millisecond
+          Thread.sleep(1000 / 30);
+
+        } catch (Exception ex) {
+          // print stack trace or do other stuffs
         }
+        // }
         return null;
       }
 
@@ -82,6 +127,19 @@ public class Sample_controller implements Initializable {
     new Thread(t).start();
 
   }
+
+  /*
+   * public static String imgToBase64String(final RenderedImage img, final String
+   * formatName) { final ByteArrayOutputStream os = new ByteArrayOutputStream();
+   * try { ImageIO.write(img, formatName, Base64.getEncoder().wrap(os)); return
+   * os.toString(StandardCharsets.ISO_8859_1.name()); } catch (final IOException
+   * ioe) { throw new UncheckedIOException(ioe); } }
+   * 
+   * public static BufferedImage base64StringToImg(final String base64String) {
+   * try { return ImageIO.read(new
+   * ByteArrayInputStream(Base64.getDecoder().decode(base64String))); } catch
+   * (final IOException ioe) { throw new UncheckedIOException(ioe); } }
+   */
 
   @FXML
   public void streamingStop() {
