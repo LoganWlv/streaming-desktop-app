c99a41f0637a7527a59248b3e82da9d10747c503
LoganWlv <gm.rlogan@gmail.com>
2018-09-20 20:01:00 +0200

    Test upd

--- interface/src/main/java/com/SDA/admin/Sample_controller.java
+++ interface/src/main/java/com/SDA/admin/Sample_controller.java
@@ -3,19 +3,19 @@ package com.SDA.admin;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.application.Platform;
+import javax.imageio.ImageIO;
+
 import javafx.concurrent.Task;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
 
 public class Sample_controller implements Initializable {
 
@@ -33,17 +33,22 @@ public class Sample_controller implements Initializable {
   @FXML
   public void streamingStart(ActionEvent event) {
     System.out.println("Starting streaming...");
-    /*
-     * for (int i = 0; i < 100; i++) { try { System.out.println("Thread start..");
-     * Robot robot = new Robot(); Rectangle box = new
-     * Rectangle(Toolkit.getDefaultToolkit().getScreenSize()); BufferedImage pic =
-     * robot.createScreenCapture(box); Image myPic = SwingFXUtils.toFXImage(pic,
-     * null); screen_display.setImage(myPic);
-     * System.out.println("Thread sleeping.."); Thread.currentThread().sleep(5 *
-     * 1000); } catch (Exception e) { System.out.println("Error: " + e); }
-     * 
-     * }
-     */
+
+    /*for (int i = 0; i < 100; i++) {
+      try {
+        System.out.println("Thread start..");
+        Robot robot = new Robot();
+        Rectangle box = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        BufferedImage pic = robot.createScreenCapture(box);
+        Image myPic = SwingFXUtils.toFXImage(pic, null);
+        screen_display.setImage(myPic);
+        System.out.println("Thread sleeping..");
+        Thread.currentThread().sleep(5 * 1000);
+      } catch (Exception e) {
+        System.out.println("Error: " + e);
+      }
+    
+    }*/
 
     Task t = new Task() {
       @Override
@@ -54,18 +59,20 @@ public class Sample_controller implements Initializable {
             Robot ro = new Robot();
             BufferedImage im = ro
                 .createScreenCapture(new java.awt.Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-            final WritableImage image = new WritableImage(500, 500);
-            final Image myPic = SwingFXUtils.toFXImage(im, null);
-
+            byte[] imBytes = toByteArrayAutoClosable(im, "jpg");
+            for (int i = 0; i < imBytes.length; i++) {
+              System.out.println(imBytes[i]);
+            }
+            /*
             // Calling the JavaFX Thread to update javafx control
             Platform.runLater(new Runnable() {
               public void run() {
                 // This updates the imageview to newly created Image
                 System.out.println("Print toto");
-
+            
                 screen_display.setImage(myPic);
               }
-            });
+            });*/
 
             // Sleep for 100 millisecond
             Thread.sleep(1000 / 30);
@@ -90,8 +97,15 @@ public class Sample_controller implements Initializable {
   }
 
   public void initialize(URL location, ResourceBundle resources) {
-    // TODO Auto-generated method stub
+  }
 
+  private static byte[] toByteArrayAutoClosable(BufferedImage image, String type) throws IOException {
+    try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+      System.out.println("ImageIO write stream .. \n" + System.currentTimeMillis());
+      ImageIO.write(image, type, out);
+      System.out.println("ImageIO after write stream .. \n" + System.currentTimeMillis());
+      return out.toByteArray();
+    }
   }
 
 }
